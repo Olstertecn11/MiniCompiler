f5dd62757a86a49d8b10af49c60d5056eeae9855
Olstertecn11 <otzunund@miumg.edu.gt>
2023-06-08 08:30:49 -0600

    translating .isa to c++

--- src/analyzer.cpp
+++ src/analyzer.cpp
@@ -1,41 +1,17 @@
 #include "./include/analyzer.h"
 #include <string>
 
-int Analyzer::get_position(string line, string ref) {
-  for (int i = 0; i < line.length(); i++) {
-    string cadena = "";
-    for (int j = i; j < line.length(); j++) {
-      cadena += line[j];
-      if (cadena.length() == ref.length()) {
-        if (cadena == ref)
-          return i;
-        else
-          cadena = "";
-      }
-    }
-  }
-  return -1;
-}
+int Analyzer::get_position(string line, string ref) { return line.find(ref); }
 
-string Analyzer::get_new_string(std::string ref, std::string cadena) {
-  int pos = Analyzer::get_position(cadena, "si(");
-  if (pos != -1) {
-    string aux = "";
-    for (int i = 0; i < cadena.length(); i++) {
-      if (i < pos || i > pos + 2) {
-        aux += cadena[i];
-      } else {
-        aux += ref;
-        i = pos + 2;
-      }
-    }
-    return aux;
-  } else {
-    return "";
-  }
+string Analyzer::get_new_string(std::string ref, std::string cadena,
+                                std::string new_string) {
+  int index = Analyzer::get_position(cadena, ref);
+  int last = index + ref.length();
+  return cadena.substr(0, index) + new_string + cadena.substr(last);
 }
 
 void Analyzer::read_file() {
+
   ifstream archivo;
   archivo.open("file.isa", ios::in);
   if (archivo.fail()) {
@@ -46,12 +22,16 @@ void Analyzer::read_file() {
       getline(archivo, cadena);
 
       if (cadena.find("si(") != string::npos) {
-        cadena = this->get_new_string("if(", cadena);
+        cadena = this->get_new_string("si(", cadena, "if(");
       }
 
       if (cadena.find("principal(") != string::npos) {
-        cout << "here" << endl;
-        cadena = this->get_new_string("main(", cadena);
+        string aux = this->get_new_string("principal(", cadena, "int main(");
+        cadena = aux;
+      }
+      if (cadena.find("#incluir") != string::npos) {
+        string aux = this->get_new_string("#incluir", cadena, "#include");
+        cadena = aux;
       }
       cout << cadena << endl;
     }
